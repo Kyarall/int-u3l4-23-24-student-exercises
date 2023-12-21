991fcac645181e722c312c750ea8766d39f95df0
Kyarall <147104307+Kyarall@users.noreply.github.com>
2023-12-21 22:16:31 +0000

    12/21/23

--- domManipulation.js
+++ domManipulation.js
@@ -22,24 +22,27 @@ let image = document.querySelector('.image');
 //   - The `location` paragraph.
 
 contentButton.addEventListener('click', function() {
+  myName.textContent = 'Kyara Luna';
+  myHobby.textContent = 'I like to play volleyball and badminton';
+  myLocation.textContent = 'I live in Queens, New York';
 
 
 
 
 
-
-});
+});;
 
 // 3. Add an event listener to the `image1Button`
 // 4. When clicked, it should change the source of `image` to another image URL.
 
-
-
-
-
+image1Button.addEventListener('click', function() {
+  image.src = "https://wallpapers.com/images/hd/ugly-rat-pictures-htkgui2lbr935i8r.jpg"
+});
 // 5. Add an event listener to the `image2Button`
 // 6. When clicked, it should change the source of `image` to another image URL.
-
+image2Button.addEventListener('click', function() {
+  image.src = "https://wallpapers.com/images/hd/ugly-rat-pictures-htkgui2lbr935i8r.jpg"
+});
 
 // BONUS
 // 7. Try using different event types for your buttons.
--- script.js
+++ script.js
@@ -3,15 +3,15 @@ let button = document.querySelector("button");
 let img = document.querySelector("img");
 
 // 1. Press the button on the webpage to change it up! Take notice of what happens.
-// - What do you think innerHTML does?
-// - What do you think .src does?
+// - What do you think innerHTML does? I think it puts the text in the html.
+// - What do you think .src does? I think it puts the image in the html.
 
 
 // 2. Try the following:
 // - Make it so that the header says your name when the button is clicked.
 // - Change the src of the image to something random.
 button.addEventListener("click", function() {
-    header.innerHTML = "🤯 wow cool neeto";
+    header.innerHTML = "🤯 Kyara";
     img.src = "https://media4.giphy.com/media/26ufdipQqU2lhNA4g/giphy.gif";
 });
 
